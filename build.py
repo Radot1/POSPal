772cd7f9b89f561f66de3f1dae92be11f83083a6
R. Zaire <bzoumboulis@yahoo.co.uk>
2025-06-28 12:27:06 +0300

    actions etc

--- build.py
+++ build.py
@@ -1,4 +1,12 @@
 import PyInstaller.__main__
+import os
+import shutil
+
+# Clean previous builds
+if os.path.exists('dist'):
+    shutil.rmtree('dist')
+if os.path.exists('build'):
+    shutil.rmtree('build')
 
 PyInstaller.__main__.run([
     'app.py',
