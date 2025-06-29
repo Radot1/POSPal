7aa77402e0f18af38cd17978bd23a50dc74c6586
R. Zaire <bzoumboulis@yahoo.co.uk>
2025-06-29 11:05:44 +0300

    version number 1.0.1

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-CURRENT_VERSION = "1.0.0"  # Update this with each release
+CURRENT_VERSION = "1.0.1"  # Update this with each release
 
 from flask import Flask, request, jsonify, send_from_directory
 from datetime import datetime
