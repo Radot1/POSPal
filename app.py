06289ba6b4fad20088416ec61788b66dd724375c
Radot1 <bzoumboulis@yahoo.co.uk>
2025-08-19 22:50:20 +0300

    V1.0.6

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-CURRENT_VERSION = "1.0.5"  # Update this with each release
+CURRENT_VERSION = "1.0.6"  # Update this with each release
 
 from flask import Flask, request, jsonify, send_from_directory
 from datetime import datetime, timedelta
